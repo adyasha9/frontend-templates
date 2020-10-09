96d4bce5a96ed7325e53d0ae6f62a44590ee55e6
Ranabir Basu <ranabirbasu12@gmail.com>
2020-10-10 01:50:35 +0530

    Task 2 done.

--- Task2/script.js
+++ Task2/script.js
@@ -1,9 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* Toggle between adding and removing the "responsive" class to naval when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    if (x.className === "naval") {
         x.className += " responsive";
     } else {
-        x.className = "topnav";
+        x.className = "naval";
     }
 }
